7d24dad1e3c50e51d35e6c4eb2619744f38cfe3c
April Xu <aprilxu2222@gmail.com>
2017-08-04 11:30:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,18 +71,6 @@ function init() {
       openWindow("windows/setup.html");
     }
   });
-  
-  chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    return {
-      responseHeaders: details.responseHeaders.filter(function(header) {
-        return (header.name.toLowerCase() !== 'x-frame-options');
-      })
-    };
-  }, {
-    urls: ["<all_urls>"]
-  }, ["blocking", "responseHeaders"]);
-
   chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
     if(request == "reload"){
       chrome.runtime.getPlatformInfo(function(p){
